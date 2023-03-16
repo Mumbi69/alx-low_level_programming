9d774711c7e1bea2cff12a9bc0678b223e4cb6bb
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-16 19:51:28 +0000

    My C codes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ * Main - start of the program
+ *
+ * return: zero
+ */
+
 int main(void)
-	 /* Main fuction */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
