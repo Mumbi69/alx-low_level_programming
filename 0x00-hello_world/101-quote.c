82af73072496c7bb1394bb945f45c34bf2e72c24
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-16 22:20:01 +0000

    My C codes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	const char= "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n";
-	write(2, message, 59);
+	write(2,
+		"and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n")
 	return (1);
 
 }
