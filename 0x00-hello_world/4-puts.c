18e49b5c3bffcd8b570da1f98f061e0789e1a46f
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-16 19:25:18 +0000

    My C codes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 int main(void)
-		/**
-	 * This is the preferred style for multi-line
-	 * comments in C source code.
-	 * Please use it consistently.
-	 *
-	 * Description:  A column of asterisks on the left side,
-	 * with beginning and ending almost-blank lines.
-	 */
+	 /* Main fuction */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
