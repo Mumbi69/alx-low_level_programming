e2c2c24786f550dc012f404a6e3ef4ee0f25566c
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-17 19:35:07 +0000

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - starts a program
+*
+* Return: returns 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+
+		putchar(',');
+
+		putchar(' ');
+}
+
+		putchar('\n');
+
+	return (0);
+}
