206e54655a4a9e57fa797b866d56abe109ec4cee
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-21 17:11:48 +0000

    Functions, nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,4 +14,11 @@ void print_alphabet(void);
 * prints the alphabet, in lowercase, folloed by a new line.
 *
 */
+
+void print_alphabet_x10(void);
+/**
+* prints 10 times the alphabet, in lowercase, followed by a new line.
+*
+*/
 #endif
+
