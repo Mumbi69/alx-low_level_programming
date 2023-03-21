4328e7eb9ab5def78d1dabb2b4b9d906beeac2a7
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-21 16:39:00 +0000

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main - start of a program
+*
+* Return: returns 0
+*/
+
+void print_alphabet(void)
+{
+	char m;
+
+/* print lowercase alphabet */
+	for (m = 'a'; m <= 'z'; ++m)
+	{
+
+		_putchar(m);
+
+	}
+
+	_putchar('\n');
+
+}
