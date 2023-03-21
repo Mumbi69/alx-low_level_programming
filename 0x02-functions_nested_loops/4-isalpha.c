0c2d2454efea9a1693c334f91d618491eae9e6c8
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-21 17:41:34 +0000

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _isalpha - checks for alphabetic character
+*
+* Return: On success returns 1, on error returns 0.
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return 0;
+	}
+
+
+
+}
