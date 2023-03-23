ffc2da6fc9d6ab60b6160e54607a758f1bb822aa
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-23 19:57:44 +0000

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+* _isupper - start of a program
+*
+* Return: returns 1 if c is uppercase otherwise 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
