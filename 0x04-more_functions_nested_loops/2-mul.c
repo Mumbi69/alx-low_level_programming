0fb7a1f554a258834ac5edbe78617c7205e7cf17
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-23 20:23:21 +0000

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,9 @@
 /**
- * _mul - check the code.
- *
- * Return: Always 0.
- */
+* mul - multiplies two integers
+*@a: The first integer
+* @b: The second integer
+* Return: Always 0.
+*/
 int mul(int a, int b)
 {
 	return (a * b);
