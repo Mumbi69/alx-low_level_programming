0f50937446359e97e4d642d947c77129bc9565c4
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-27 17:36:38 +0000

    More funtions, more nested loops

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
-#include <main.h>
+#include "main.h"
 
 /**
-* _reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98.
-* @n: The integer to point to
+* _reset_to_98 - takes a pointer to an int as parameter and
+* updates the value it points to to 98.
+* @n: The integer to point to point to
 */
 
 void reset_to_98(int *n);
 {
-	int c;
-
-	*c = 98;
+	*n = 98;
 }
