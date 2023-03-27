1190cd0ef9b6b860886e355377a1f657b319a45e
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-27 18:25:34 +0000

    More funtions, more nested loops

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_rev - prints a string,in reverse,followed by a new line.
+* @s: the string
+*
+* Return: returns 0 always
+*/
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	while (len > 0)
+	{
+		_putchar(*s);
+		s--;
+		len--;
+	}
+
+	_putchar('\n');
+}
