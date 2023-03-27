da7c112267d4e42c62ccd05e003f7508782c8c93
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-27 21:28:07 +0000

    More funtions, more nested loops

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,27 +4,45 @@
 
 #define PASSWORD_LENGTH 10
 
+/**
+* main - starts the program
+*
+* Return: always retun 0
+*/
+
 int main(void)
 {
-	char password[PASSWORD_LENGTH];
+	char password[PASSWORD_LENGTH + 1];
 	int i;
 
 	srand(time(NULL));
 
 	for (i = 0; i < PASSWORD_LENGTH; i++)
 	{
-		int r = rand() % 62;
 
-		if (r < 26)
-		{
-			password[i] = 'A' + r;
-		}
-		else if (r < 52)
-		{
-			password[i] = 'a' + r - 26;
-		}
-		else
-		{
-			password[i] = '0' + r - 52;
+	int r = rand() % 93;
+
+	if (r < 33)
+	{
+		password[i] = '!' + r;
 	}
+	else if (r < 59)
+	{
+		password[i] = 'A' + r - 33;
+	}
+	else if (r < 85)
+	{
+		password[i] = 'a' + r - 59;
+	}
+	else
+	{
+		password[i] = '0' + r - 85;
+	}
+}
+
+	password[PASSWORD_LENGTH] = '\0';
+
+	printf("Random password: %s\n", password);
+
+	return (0);
 }
