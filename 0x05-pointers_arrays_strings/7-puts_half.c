eddfa44f7b9e473817afb877e292e2eafdef821a
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-27 19:11:15 +0000

    More funtions, more nested loops

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* puts_half - prints half of a string,followed by a new line.
+* @str: the half string
+*
+* Return: always return 0
+*/
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+
+	_putchar('\n');
+}
