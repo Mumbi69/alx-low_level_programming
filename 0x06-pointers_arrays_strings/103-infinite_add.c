da8b50f5106b92db1b70b22cc68433739441445e
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-30 19:14:13 +0000

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,7 +8,7 @@
 * @r: ...
 * @size_r: ...
 *
-* Returm: 0
+* Return: 0
 */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
@@ -19,24 +19,29 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int i = len1 - 1, j = len2 - 1, k = 0;
 
 	while (i >= 0 || j >= 0 || carry > 0)
-        {
+	{
 		sum = carry;
-        	if (i >= 0) sum += n1[i--] - '0';
-        	if (j >= 0) sum += n2[j--] - '0';
-        	if (k >= size_r) return 0;
-        	r[k++] = sum % 10 + '0';
-        	carry = sum / 10;
+		if (i >= 0)
+			sum += n1[i--] - '0';
+		if (j >= 0)
+			sum += n2[j--] - '0';
+		if (k >= size_r)
+			return (0);
+		r[k++] = sum % 10 + '0';
+		carry = sum / 10;
 	}
 
-	if (k >= size_r) return 0;
+	if (k >= size_r)
+		return (0);
 	r[k] = '\0';
 
 	for (i = 0, j = k - 1; i < j; i++, j--)
-        {
+	{
 		char temp = r[i];
+
 		r[i] = r[j];
 		r[j] = temp;
-	}
 
-	return r;
+	}
+	return (r);
 }
