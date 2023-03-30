af6fc109895d0bdee2fb0ca1c7f725d221396676
Mumbi69 <winfriendmumbi69@gmail.com>
2023-03-30 10:22:56 +0000

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* _strcat - concatinates two strings
+* @dest: first string
+* @src: second string
+*
+* Return: returns a pointer to the resulting dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
