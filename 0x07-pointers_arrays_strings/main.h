45ae709079b5d4bd1e820e86e390474904a71b06
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-03 10:01:18 +0000

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,30 @@
+#ifndef Mumbi_header
+#define Mumbi_header
+
+int _putchar(char c);
+
+/**
+* _ putchar - writes the character c to stdout
+* @c: The character to print
+*
+*/
+
+char *_memset(char *s, char b, unsigned int n);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_strchr(char *s, char c);
+
+unsigned int _strspn(char *s, char *accept);
+
+char *_strpbrk(char *s, char *accept);
+
+char *_strstr(char *haystack, char *needle);
+
+void print_chessboard(char (*a)[8]);
+
+void print_diagsums(int *a, int size);
+
+void set_string(char **s, char *to);
+
+#endif
