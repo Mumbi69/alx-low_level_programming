974535470ef249efebaba2145fb33adc3a4814fd
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-05 21:47:18 +0000

    Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,8 +15,8 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	}
 	if (*s2 == '*')
-    	{
-        	if (wildcmp(s1, s2 + 1) == 1)
+	{
+		if (wildcmp(s1, s2 + 1) == 1)
 			return (1);
 
 		if (*s1 != '\0' && wildcmp(s1 + 1, s2) == 1)
