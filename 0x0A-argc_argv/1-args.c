8b877ad15f2571b72a9621f32f6faaad83fbf6c7
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-08 11:16:34 +0000

    argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,15 +3,15 @@
 /**
 * main - main function
 * _args - argument
-* @argc - number of arguments
-* @argv - array of strings
+* @argc: number of arguments
+* @argv: array of strings
 *
 * Return: 0
 */
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%d\n", argc);
 	return (0);
