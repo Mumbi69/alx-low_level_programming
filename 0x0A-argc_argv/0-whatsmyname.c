31c006363a94937cd281d559158416b6bd75a7ce
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-08 10:57:00 +0000

    argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _whatsmyname -  prints its name
+* @argc: number of arguments passed
+* @argv: one-dimensional array of strings
+*
+* Return: 0
+*/
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
