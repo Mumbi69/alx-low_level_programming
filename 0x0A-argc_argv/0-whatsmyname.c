d430e5cfa0f3f69c5b6ead91e3304416a35a5fda
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-08 11:08:16 +0000

    argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 /**
+* main - main function
 * _whatsmyname -  prints its name
 * @argc: number of arguments passed
 * @argv: one-dimensional array of strings
@@ -10,7 +11,7 @@
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
