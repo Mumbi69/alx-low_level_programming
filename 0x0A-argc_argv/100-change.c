5f651344da898f46649d114dc8fd2c5d4bb4a75a
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-10 13:39:37 +0000

    argc, argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,37 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    int cents;
-    int coins = 0;
+/**
+* main - main function
+* @argc: number of arguments
+* @argv: array of strings
+* _change: minimum number of coins to make change
+*
+* Return: 0
+*/
 
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
+int main(int argc, char *argv[])
+{
+	int cents;
+	int coins = 0;
 
-    cents = atoi(argv[1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    if (cents < 0) {
-        printf("0\n");
-        return 0;
-    }
+	cents = atoi(argv[1]);
 
-    coins += cents / 25;
-    cents %= 25;
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    coins += cents / 10;
-    cents %= 10;
+	coins += cents / 25;
+	cents %= 25;
 
-    coins += cents / 5;
-    cents %= 5;
+	coins += cents / 10;
+	cents %= 10;
 
-    coins += cents / 2;
-    cents %= 2;
+	coins += cents / 5;
+	cents %= 5;
 
-    coins += cents;
+	coins += cents / 2;
+	cents %= 2;
 
-    printf("%d\n", coins);
+	coins += cents;
 
-    return 0;
+	printf("%d\n", coins);
+
+	return (0);
 }
