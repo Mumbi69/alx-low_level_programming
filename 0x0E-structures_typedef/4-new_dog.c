bfa3d040cf282bacbfcab7069a28ea7e3773acb4
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-17 18:51:02 +0000

    Structures, typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* new_dog - creates a new struct of type dog
+* @name: ...
+* @age: ...
+* @owner: ...
+*
+* Return: pointer to buffer of datatype dog
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+	{
+        	return (NULL);
+	}
+
+	dog->name = malloc(strlen(name) + 1);
+
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	strcpy(dog->name, name);
+
+	dog->owner = malloc(strlen(owner) + 1);
+
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	strcpy(dog->owner, owner);
+
+	dog->age = age;
+
+	return (dog);
+}
