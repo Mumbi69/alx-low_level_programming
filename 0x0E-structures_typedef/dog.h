f0bd501b1ebd613dd922573eb1311bbc7591358c
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-17 18:19:46 +0000

    Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,20 @@
 #ifndef Mumbi_header
 #define Mumbi_header
 
-/**
-* struct dog - main function
-* @name: ...
-* @age: ...
-* @owner: ...
-*
-* Return: 0
-*/
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
