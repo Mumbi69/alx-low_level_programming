e4ba2e262d354ea7569b1b63d8af84e24632caa3
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-17 18:23:24 +0000

    Structures, typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <string.h>
+#include <stdlib.h>
 
 /**
 * struct dog - main function
@@ -13,7 +13,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-		strcpy(d->name, name);
+	if (d)
+	{
+		d->name = name;
 		d->age = age;
-		strcpy(d->owner, owner);
+		d->owner = owner;
+	}
 }
