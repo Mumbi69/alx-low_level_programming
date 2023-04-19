44eb0f47197299e97823109401f74ec0605a20fd
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-19 13:53:13 +0000

    Function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,25 @@
-#ifndef function_pointers_H
-#define function_pointers_H
+#ifndef function_pointers_header
+#define function_pointers_header
 
 void print_name(char *name, void (*f)(char *));
 
 int _putchar(char c);
 
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+int op_add(int a, int b);
+
+int op_sub(int a, int b);
+
+int op_mul(int a, int b);
+
+int op_div(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
+
+
+
 #endif
+
