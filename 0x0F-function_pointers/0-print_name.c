0cd7e3f4ae17a8ee6fcddde9c4d94823d03136f6
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-19 18:51:14 +0000

    Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <string.h>
+#include "function_pointers.h"
+#include <stdlib.h>
 
 /**
 * print_name -  prints a name.
@@ -14,30 +14,3 @@ void print_name(char *name, void (*f)(char *))
 	f(name);
 }
 
-/**
-* print_normal - prints normal
-* @name: ...
-* Return: nothing
-*/
-
-void print_normal(char *name)
-{
-	printf("Hello, my name is %s\n", name);
-}
-
-/**
-* print_reversed - prints reversed
-* @name: ...
-* Return: nothing
-*/
-void print_reversed(char *name)
-{
-	size_t i;
-
-	printf("My name spelled backwards is ");
-	for (i = strlen(name); i > 0; i--)
-	{
-		printf("%c", name[i - 1]);
-	}
-	printf("\n");
-}
