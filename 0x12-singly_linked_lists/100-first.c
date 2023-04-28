30658a1a35761786cd36f68e324bb92087ec67c9
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-28 00:48:32 +0000

    Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* print_message - prints message before the main function
+*
+*
+* Return: 0
+*/
+
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+
+}
