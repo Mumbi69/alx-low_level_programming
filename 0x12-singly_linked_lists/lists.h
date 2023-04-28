12b09971102ff01966885f8fb834abe9f4d6ead5
Mumbi69 <winfriendmumbi69@gmail.com>
2023-04-28 00:53:42 +0000

    Singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -29,9 +29,7 @@ list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
 
-void print_message();
-
-int main (void);
+void print_message(void) __attribute__((constructor));
 
 #endif
 
