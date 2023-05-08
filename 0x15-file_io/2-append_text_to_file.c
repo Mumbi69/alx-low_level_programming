17dd9e8e67d7836516b7574838a25bff9dad192e
Mumbi69 <winfriendmumbi69@gmail.com>
2023-05-08 20:32:19 +0000

    C - File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,25 +2,32 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text at the end of a file
- * @filename: Name of the file to append to
- * @text_content: Text to append to the file
- *
- * Return: 1 on success, -1 on failure
- */
+* append_text_to_file - Appends text at the end of a file
+* @filename: Name of the file to append to
+* @text_content: Text to append to the file
+*
+* Return: 1 on success, -1 on failure
+*/
 
-int append_text_to_file(const char *filename, char *text_content) {
-    if (filename == NULL) {
-        return -1;
-    }
-    if (text_content == NULL) {
-        return 1;
-    }
-    FILE *fp = fopen(filename, "a");
-    if (fp == NULL) {
-        return -1;
-    }
-    fprintf(fp, "%s", text_content);
-    fclose(fp);
-    return 1;
+int append_text_to_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+
+	fp = fopen(filename, "a");
+	if (fp == NULL)
+	{
+		return (-1);
+	}
+	fprintf(fp, "%s", text_content);
+	fclose(fp);
+	return (1);
 }
