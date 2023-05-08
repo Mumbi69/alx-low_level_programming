24c88736d31847e96545fd5d5ec66c00ab653015
Mumbi69 <winfriendmumbi69@gmail.com>
2023-05-08 21:05:00 +0000

    C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,16 +4,13 @@
 #include <fcntl.h>
 #include <errno.h>
 #include "main.h"
-
 #define BUF_SIZE 1024
-
 /**
 * _cp - copies the content of a file to another file
 * @argc: ...
 * @argv: ...
 * Return: 0
 */
-
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
@@ -25,21 +22,18 @@ int main(int argc, char *argv[])
 	dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 	exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
-
 	while ((num_read = read(fd_from, buf, BUF_SIZE)) > 0)
 	{
 		num_written = write(fd_to, buf, num_read);
@@ -49,24 +43,20 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
 	if (num_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-
 	return (0);
 }
