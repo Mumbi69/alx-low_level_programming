7da5e5b047dff289232d32e3ce57d8d9c54a90ce
Mumbi69 <winfriendmumbi69@gmail.com>
2023-05-26 15:51:18 +0000

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,50 +1,41 @@
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
+
 /**
 * read_textfile - reads a text file and prints
-* @filename: ...
-* @letters: ...
+* @filename: the name of the file
+* @letters: no. of characters to read
 *
 * Return: return 0
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
+	int f;
+	ssize_t byr, byw;
 	char *buffer;
-	ssize_t bytes_read, bytes_written;
 
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (0);
-	}
-	file = fopen(filename, "r");
-	if (file == NULL)
-	{
-		return (0);
-	}
-	buffer = (char *) malloc(letters + 1);
-	if (buffer == NULL)
-	{
-		fclose(file);
-		return (0);
-	}
-	bytes_read = fread(buffer, 1, letters, file);
-	if (bytes_read == -1)
-	{
-		fclose(file);
-		free(buffer);
-		return (0);
-	}
-	bytes_written = fwrite(buffer, 1, bytes_read, stdout);
-	if (bytes_written != bytes_read)
-	{
-		fclose(0);
-		free(buffer);
-		return (0);
-	}
 
-	fclose(file);
+	f = open(filename, O_RDONLY);
+
+	if (f == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+
+	byr = read(f, buffer, letters);
+	byw = write(STDOUT_FILENO, buffer, byr);
+
+	close(f);
+
 	free(buffer);
-	return (bytes_read);
+
+	return (byw);
 }
