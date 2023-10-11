500b63241a0e1982e10b2077f8898c4318641a2a
Mumbi69 <winfriendmumbi69@gmail.com>
2023-10-11 23:32:52 +0000

    Search Algorithms

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -21,8 +21,7 @@ int interpolation_search(int *array, size_t size, int value)
 
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
-		p = low + (((double)(high - low) / (array[high] - array[low])) * /
-			(value - array[low]));
+		p = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 
 		if (p < 0 || p >= (int)size)
 		{
